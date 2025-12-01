e00d415b42ddeaf41a0815d13cc1a9287a351277
YeshwantRaoB <byeshwantrao@gmail.com>
2025-12-01 22:27:42 +0530

    added react router

---
+++ src/testing/router/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Navbar from "./Navbar";
+
+function App() {
+    return (
+<div className="App">
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+    </Routes>
+    </BrowserRouter>
+    </div>
+    );
+}
+
+export default App;
---
+++ src/testing/router/Contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <p>This is the Contact page of our website.</p>
+        </div>
+    );
+};
+
+export default Contact;
